5178e2ef2755918d94c61de01ab365d2f77ec15a
Leonard <leonardwkee@gmail.com>
2021-08-26 23:18:55 -0700

    update component hierarchy & clean up functionality

---
+++ src/components/Generations.tsx
@@ -0,0 +1,69 @@
+import React, {
+  FunctionComponent, ReactElement, useState,
+} from 'react'
+import { useHistory } from 'react-router'
+import Drawer from '@material-ui/core/Drawer'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+
+interface GenerationsProps {
+  genList: any[]
+  // eslint-disable-next-line no-unused-vars
+  getPokemonData: (name: string) => Promise<void>
+}
+
+const Generations: FunctionComponent<GenerationsProps> = ({ genList, getPokemonData }: GenerationsProps) => {
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false)
+  const [generation, setGeneration] = useState<number | string>(0)
+  const history = useHistory()
+
+  // switch between generation list and generation's pokemon list
+  const list = (): ReactElement => {
+    const arr = generation ? genList[generation as number - 1] : genList
+    const clickListener = async (gen: number | string) => {
+      if (!generation) {
+        return setGeneration(gen as number + 1)
+      }
+      history.push(`/${gen}`, { updated: true })
+      setOpenDrawer(false)
+      await getPokemonData(gen as string)
+    }
+    return (
+      <div role="presentation" onKeyDown={() => setOpenDrawer(false)}>
+        <List>
+          {arr?.map((el: Record<any, any>, index: number) => (
+            <ListItem button key={index} onClick={() => clickListener(generation ? el.name : index)}>
+              <ListItemText primary={generation ? el.name : `Gen ${index + 1}`} />
+              {!generation && <ChevronRightIcon />}
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Button onClick={() => setOpenDrawer(true)}>View By Generation</Button>
+      <Drawer
+        anchor="left"
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      >
+        {generation > 0 && (
+          <ListItem button onClick={() => setGeneration(0)}>
+            <ListItemText primary="Back to main menu" />
+            <ChevronLeftIcon />
+          </ListItem>
+        )}
+        {list()}
+      </Drawer>
+    </>
+  )
+}
+
+export default Generations
