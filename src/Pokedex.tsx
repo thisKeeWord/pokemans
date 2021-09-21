d8406a6fc1f2e476e07f66f547b6783abe9189fd
Leonard <leonardwkee@gmail.com>
2021-09-20 23:51:45 -0700

    add nav

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -1,6 +1,12 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
 import { useLocation, useParams } from 'react-router'
 import axios from 'axios'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
 import Generations from './components/Generations'
 import PokemonEntry from './components/PokemonEntry'
 
@@ -74,10 +80,26 @@ const Pokedex: FunctionComponent = () => {
 
   return (
     <div className="pokeList">
-      <h3 className="title">Pokemans</h3>
-      {error && <span>{error}</span>}
-      <Generations genList={genList} state={state} />
-      <PokemonEntry pokemonEntry={pokemonEntry} />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <Generations genList={genList} state={state} />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Pokemans
+            </Typography>
+            <Link href="/about">About</Link>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      {/* {error && <span>{error || 'yo'}</span>} */}
     </div>
   )
 }
