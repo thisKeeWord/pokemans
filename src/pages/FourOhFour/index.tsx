3ff433f16bd51bb7425cf492f9ad1e84aabf9072
Leonard <leonardwkee@gmail.com>
2021-09-30 21:25:06 -0700

    add test for FourOhFour page

--- src/pages/FourOhFour/index.tsx
+++ src/pages/FourOhFour/index.tsx
@@ -2,15 +2,15 @@ import React, { FunctionComponent } from 'react'
 
 const FourOhFour: FunctionComponent = () => (
   <div className="four-oh-four-body">
+    <div className="pikachu-gif">
+      <img alt="pikachu" data-testid="pikachu" />
+    </div>
+    <p>It looks like you got lost in Diglett&apos;s Tunnel.</p>
     <div data-testid="four-oh-four-body">
-      <div className="pikachu-gif">
-        <img alt="pikachu" data-testid="pikachu" />
-      </div>
-      <p>It looks like you got lost in Diglett&apos;s Tunnel.</p>
       <span>
-        Use the Escape Rope
+        Use this free
         {' '}
-        <a href="/"><strong>here</strong></a>
+        <a href="/" data-testid="home-link"><strong>Escape Rope</strong></a>
         {' '}
         to return to the nearest saved point.
       </span>
