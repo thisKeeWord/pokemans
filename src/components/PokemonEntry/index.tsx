7ab016aff06107e564a8a3f82216319e13278b03
Leonard <leonardwkee@gmail.com>
2021-10-13 19:51:00 -0700

    add key prop to elements

--- src/components/PokemonEntry/index.tsx
+++ src/components/PokemonEntry/index.tsx
@@ -21,74 +21,77 @@ const PokemonEntry: FunctionComponent<PokemonEntryProps> = ({ pokemonEntry, poke
   }
 
   return (
-    <div data-testid="pokemon-information">
+    <div data-testid="pokemon-information" key={pokemonEntry.name}>
       <div className="onTop pokemon-content">
         <h2 className="pokemon-name-image">
           <img src={pokemonEntry.sprites?.front_default} alt={pokemonEntry.sprites?.front_default} />
           {pokemonEntry.name}
         </h2>
       </div>
-      <div className="pokemon-content">
-        <Table className="table">
-          <TableBody>
-            <TableRow className="descriptor first-row" key="evolution">
+      <div className="pokemon-content" key={pokemonEntry.name}>
+        <Table className="table" key={pokemonEntry.name}>
+          <TableBody key={pokemonEntry.name}>
+            <TableRow className="descriptor first-row" key={pokemonEntry.name}>
               <TableCell className="key" id="information">Part Of Evolutionary Chain</TableCell>
               <TableCell className="key">{pokemonEvolutionEntry.chain?.evolves_to.length > 0 ? 'Yes' : 'No' }</TableCell>
             </TableRow>
-            {Object.keys(pokemonEntry).map((key) => {
+            {Object.keys(pokemonEntry).map((key, index) => {
             // eslint-disable-next-line max-len
               if (!!pokemonEntry[key] && (categories.includes(key))) {
                 if (Array.isArray(pokemonEntry[key])) {
                   return (
-                    <TableRow className="descriptor">
-                      <TableCell className="key" id="information">
+                    <TableRow className="descriptor" key={key + index}>
+                      <TableCell className="key" id="information" key={key + index}>
                         {key.replace(/-/g, ' ')}
                       </TableCell>
                       <TableCell className="key">
                         {pokemonEntry[key].map((elem, idx) => {
                           if (elem.move) {
                             if (idx === pokemonEntry[key].length - 1) {
-                              return <span className="key">{elem.move.name.replace(/-/g, ' ')}</span>
+                              return <span className="key" key={elem + idx}>{elem.move.name.replace(/-/g, ' ')}</span>
                             }
 
                             return (
-                              <span className="trait">
+                              <span className="trait" key={elem + idx}>
                                 {elem.move.name.replace(/-/g, ' ')}
                                 ,
                                 {' '}
                               </span>
                             )
                           }
+
                           if (elem.name) {
                             if (idx === pokemonEntry[key].length - 1) {
-                              return <span className="key">{elem.name}</span>
+                              return <span className="key" key={elem + idx}>{elem.name}</span>
                             }
 
                             return (
-                              <span className="trait">
+                              <span className="trait" key={elem + idx}>
                                 {elem.name}
                                 ,
                                 {' '}
                               </span>
                             )
                           }
+
                           if (elem.ability) {
                             if (idx === pokemonEntry[key].length - 1) {
-                              return <span className="key">{elem.ability.name}</span>
+                              return <span className="key" key={elem + idx}>{elem.ability.name}</span>
                             }
 
                             return (
-                              <span className="trait">
+                              <span className="trait" key={elem + idx}>
                                 {elem.ability.name}
                                 ,
                                 {' '}
                               </span>
                             )
                           }
+
                           if (elem.stat) {
                             if (idx === pokemonEntry[key].length - 1) {
                               return (
-                                <span className="trait">
+                                <span className="trait" key={elem + idx}>
                                   {elem.stat.name}
                                   :
                                   {' '}
@@ -98,7 +101,7 @@ const PokemonEntry: FunctionComponent<PokemonEntryProps> = ({ pokemonEntry, poke
                             }
 
                             return (
-                              <span className="trait">
+                              <span className="trait" key={elem + idx}>
                                 {elem.stat.name}
                                 :
                                 {' '}
@@ -107,13 +110,14 @@ const PokemonEntry: FunctionComponent<PokemonEntryProps> = ({ pokemonEntry, poke
                               </span>
                             )
                           }
+
                           if (elem.type) {
                             if (idx === pokemonEntry[key].length - 1) {
-                              return <span className="trait">{elem.type.name.replace(/-/g, ' ')}</span>
+                              return <span className="trait" key={elem + idx}>{elem.type.name.replace(/-/g, ' ')}</span>
                             }
 
                             return (
-                              <span className="trait">
+                              <span className="trait" key={elem + idx}>
                                 {elem.type.name.replace(/-/g, ' ')}
                                 ,
                                 {' '}
@@ -129,7 +133,7 @@ const PokemonEntry: FunctionComponent<PokemonEntryProps> = ({ pokemonEntry, poke
                 }
 
                 return (
-                  <TableRow className="descriptor">
+                  <TableRow className="descriptor" key={`${key}unique`}>
                     <TableCell className="key" id="information">{key.replace(/_/g, ' ')}</TableCell>
                     <TableCell className="key">{pokemonEntry[key].toString()}</TableCell>
                   </TableRow>
